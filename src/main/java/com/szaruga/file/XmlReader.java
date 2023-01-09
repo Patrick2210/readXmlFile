d62acd6cb3b1fbd8fe1e498af2cf71d9d305702c
szaruga92 <szaruga92@gmail.com>
2023-01-09 13:38:45 +0000

    projcet

--- src/main/java/com/szaruga/file/XmlReader.java
+++ src/main/java/com/szaruga/file/XmlReader.java
@@ -1,9 +1,6 @@
 package com.szaruga.file;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
