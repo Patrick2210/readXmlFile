55aaecd49a7a2813979afe2007398256f6411cb2
szaruga92 <szaruga92@gmail.com>
2023-01-18 19:04:05 +0000

    project

--- src/main/java/com/szaruga/cmd/CommandLineReader.java
+++ src/main/java/com/szaruga/cmd/CommandLineReader.java
@@ -1,18 +1,11 @@
 package com.szaruga.cmd;
 
-
 import com.szaruga.EnumStrings;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO zczytac wszystkie plki od 0 do -2(przedostatniego)
-// TODO sprawdzic extension czy file zawiera czy nie
-// TODO dodoac jezeli nie zawiera extension, format chciany np. csv
-// TODO zrobic metode ktora zwroci format
-// TODO enum output obiekt - enum TXT = zawiera .txt
-// TODO "dzieciczenie" + "interface" -> poczytac
 public class CommandLineReader {
     private final String[] args;
 
